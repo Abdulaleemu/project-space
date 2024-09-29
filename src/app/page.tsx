af5f6e2dce6c65bc003daa97f44c2e869dc9c0a2
abdulaleemu <abdulaleemuibrahim@gmail.com>
2024-09-29 00:32:46 -0700

    fix layout font issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import JBLADU from "@/public/images/jbladu.jpeg";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center  ">
-      <Image src={JBLADU} />
+      <Image src={JBLADU} alt="JbandLadu" />
     </div>
   );
 }
